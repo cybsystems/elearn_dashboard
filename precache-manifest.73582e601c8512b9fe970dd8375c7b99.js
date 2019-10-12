769f712ff3cdbd696512fd884f3c875abf95ca26
amit <amitwaghamare17@gmail.com>
2019-10-12 06:44:11 -0700

    Updates

---
+++ precache-manifest.73582e601c8512b9fe970dd8375c7b99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3941337493e4627d71dd",
+    "url": "https://reduction-admin.github.io/react-reduction/static/js/runtime~main.e779d46e.js"
+  },
+  {
+    "revision": "7dbf19d7a030d147f048",
+    "url": "https://reduction-admin.github.io/react-reduction/static/js/main.469884af.chunk.js"
+  },
+  {
+    "revision": "52e847c39791fd5d2af9",
+    "url": "https://reduction-admin.github.io/react-reduction/static/js/3.37e6fa3f.chunk.js"
+  },
+  {
+    "revision": "9fb99009f1554b716247",
+    "url": "https://reduction-admin.github.io/react-reduction/static/js/2.b52983a3.chunk.js"
+  },
+  {
+    "revision": "7dbf19d7a030d147f048",
+    "url": "https://reduction-admin.github.io/react-reduction/static/css/main.8eb4867c.chunk.css"
+  },
+  {
+    "revision": "920be28a517111f8a9fb06d967e0a429",
+    "url": "https://reduction-admin.github.io/react-reduction/index.html"
+  }
+];
